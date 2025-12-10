737c9ae231ba43ad484ab26159670bb74fbe0ab5
Medou96 <Mohamed ait oumghar>
2025-12-10 09:54:52 +0100

    Add:      eureka implementation

---
+++ eureka-project/src/test/java/com/eureka/project/EurekaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
